c3a7ed2e61b5f7c52ff934fceb35883b14a1e8f9
HaibulHasan <habibul.hasan.hira@gmail.com>
2021-01-25 14:19:04 +0600

    image uploadind made functional at api

--- app.js
+++ app.js
@@ -2,13 +2,35 @@ const express = require("express");
 var bodyParser = require("body-parser");
 require("./db");
 const app = express();
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
 
-// parse application/json
-app.use(bodyParser.json());
-app.get("/", (request, response, next) => {
+app.use("/rooms", express.static("upload/images"));
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: "./upload/images",
+  filename: (req, file, cb) => {
+    return cb(
+      null,
+      `${file.fieldname}${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+const upload = multer({
+  storage: storage,
+});
+
+app.get("/", (request, response) => {
   response.status(200).send({ success: "ok.." });
+});
+
+app.post("/:id", upload.single("profile"), (request, response) => {
+  const { query, params, body, file } = request;
+  console.log(query, params, body, file);
+  response.status(200).json({
+    success: 1,
+    profile_url: `http://localhost:8081/rooms/${request.file.filename}`,
+  });
 });
 
 try {
