013a3c0dc1f2427e025cbf49bb9338f022dcb8e4
HaibulHasan <habibul.hasan.hira@gmail.com>
2021-01-26 12:11:48 +0600

    unused codes removed

--- room/controller.js
+++ room/controller.js
@@ -2,8 +2,6 @@ const RoomService = require("./service");
 const multer = require("multer");
 const upload = require("./uploadImage");
 const path = require("path");
-const { request } = require("http");
-const { url } = require("inspector");
 exports.getRooms = async function (req, res, next) {
   const page = req.params.page ? req.params.page : 1;
   const limit = req.params.limit ? req.params.limit : 10;
@@ -20,17 +18,12 @@ exports.getRooms = async function (req, res, next) {
 };
 
 exports.saveRoom = async function (req, res, next) {
-  console.log("came here", req.file);
   try {
-    // await upload(req, res);
-    console.log(req.files);
-
     const room = await RoomService.saveRoom(req);
     return res.status(200).json({
       status: 200,
       data: room,
       message: "Succesfully Users Retrieved",
-      files: req.files.map((item) => item.filename),
     });
   } catch (e) {
     return res.status(400).json({ status: 400, message: e.message });
